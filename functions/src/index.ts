488aa56b17f95226cfc1fa5201f9ecbb9bd477e6
Michel Vollebregt <mi.vollebregt@gmail.com>
2018-05-07 14:54:39 +0200

    Code om prikbord te checken op de server draaien

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import TokenMessage = require('firebase-admin');
+import { checkPrikbord } from './check-prikbord';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -9,27 +9,42 @@ import TokenMessage = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 
-export const hello = functions.pubsub.topic('periodic-tick').onPublish(() => {
-    verzendTestbericht();
-});
+export const hello = functions.pubsub.topic('periodic-tick').onPublish(checkPrikbord);
+    // verzendTestbericht();
 
 
-function verzendTestbericht() {
+// export const onreq = functions.https.onRequest((req, res) => {
+//     checkPrikbord()
+//         .then(() => console.log('prikbord gecheckt'))
+//         .catch((err) => console.error(err));
+// });
 
-    const message = {
-        notification: {
-            title: 'Title',
-            body: 'Body'
-        },
-        topic: 'loopgroep'
-    };
+//
+// function verzendTestbericht() {
+//
+//     const message = {
+//         notification: {
+//             title: 'Title',
+//             body: 'Body'
+//         },
+//         topic: 'loopgroep'
+//     };
+//
+//     admin.messaging().send(message)
+//         .then((response) => {
+//             // Response is a message ID string.
+//             console.log('Successfully sent message:', response);
+//         })
+//         .catch((error) => {
+//             console.log('Error sending message:', error);
+//         });
+//
+//     // silent message
+//     // admin.messaging().sendToTopic(
+//     //     'loopgroep',;;;;;;;;7i8
+//     //     {data: {key: 'value'}},
+//     //     {contentAvailable: true}
+//     // ).then(() => { console.log('handled!')})
+//     //     .catch(() => console.log('handled!'));
+// }
 
-    admin.messaging().send(message)
-        .then((response) => {
-            // Response is a message ID string.
-            console.log('Successfully sent message:', response);
-        })
-        .catch((error) => {
-            console.log('Error sending message:', error);
-        });
-}
